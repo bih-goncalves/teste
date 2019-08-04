eaeb543161941a946764378f1452138eb66bd45b
bih_goncalves <bih_goncalves@hotmail.com>
2019-08-04 19:52:09 -0300

    tudo junto

---
+++ src/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+
+const server = require('http').Server(app);
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+
+app.use('/theme', express.static(__dirname + '/theme'));
+app.use('/atention', express.static(__dirname + '/atention'));
+app.get('/', (req, res) => {
+    return res.send('Hello API');
+})
+
+server.listen(8000);
